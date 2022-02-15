20a9b763ac88782cd0744f2a90ef4a8d3797df7a
Byungchan Lim <bl458@cornell.edu>
2022-02-15 17:36:22 -0500

    FEAT init mt.c

--- apps/mt.c
+++ apps/mt.c
@@ -2,13 +2,45 @@
  */
 
 #include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <egos/queue.h>
 #include <egos/context.h>
+#include <egos/queue.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+/**** THREADS AND SEMAPHORES ****/
+struct thread {
+  // your code here
+};
 
-int main(int argc, char **argv){
-	return 0;
+struct sema {
+  // your code here
+};
+
+// Global variables
+
+// Thread functions
+void thread_init() {}
+
+void thread_create(void (*f)(void *arg), void *arg, unsigned int stacksize) {}
+
+void thread_yield() {}
+
+void thread_exit() {}
+
+// Sema functions
+void sema_init(struct sema *sema, unsigned int count) {}
+
+void sema_dec(struct sema *sema) {}
+
+void sema_inc(struct sema *sema) {}
+
+bool sema_release(struct sema *sema) {}
+
+/**** TEST SUITE ****/
+int main(int argc, char **argv) {
+  thread_init();
+  // your code here
+  thread_exit();
+  return 0;
 }
