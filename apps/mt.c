341e8fcfd081f5d04a917852d0896b0ec4f08e1c
Sadman <skc86@cornell.edu>
2022-02-15 19:29:20 -0500

    finished thread_init

--- apps/mt.c
+++ apps/mt.c
@@ -9,20 +9,38 @@
 #include <string.h>
 
 /**** THREADS AND SEMAPHORES ****/
-struct thread {
-  // your code here
-};
+typedef struct thread
+{
+  /* Thread status 
+     0 = Running, 1 = Runnable, 2 = Terminated
+  */
+  int *status;
+  void *base;
+  void *stack_ptr;
+} * thread_t;
 
-struct sema {
+typedef struct sema
+{
   // your code here
-};
+} * sema_t;
+
+struct queue;
 
 // Global variables
+thread_t init_thread;
+struct queue *run_queue;
 
 // Thread functions
-void thread_init() {}
+void thread_init()
+{
+  init_thread = (thread_t)malloc(sizeof(struct thread));
+  run_queue = (struct queue *)malloc(sizeof(struct queue));
+  init_thread->base = NULL;
+}
 
-void thread_create(void (*f)(void *arg), void *arg, unsigned int stacksize) {}
+void thread_create(void (*f)(void *arg), void *arg, unsigned int stacksize)
+{
+}
 
 void thread_yield() {}
 
@@ -38,8 +56,10 @@ void sema_inc(struct sema *sema) {}
 bool sema_release(struct sema *sema) {}
 
 /**** TEST SUITE ****/
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   thread_init();
+  printf("########### \n");
   // your code here
   thread_exit();
   return 0;
