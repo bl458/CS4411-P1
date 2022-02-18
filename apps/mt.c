1c3d1afce09d691bd2bfa741ea5cc32cadaf777d
Sadman <skc86@cornell.edu>
2022-02-17 19:02:21 -0500

    added thread_create to thread_exit

--- apps/mt.c
+++ apps/mt.c
@@ -17,6 +17,7 @@ typedef struct thread
   int *status;
   void *base;
   void *stack_ptr;
+  void *function;
 } * thread_t;
 
 typedef struct sema
@@ -28,23 +29,68 @@ struct queue;
 
 // Global variables
 thread_t init_thread;
+thread_t current_thread;
 struct queue *run_queue;
 
 // Thread functions
 void thread_init()
 {
-  init_thread = (thread_t)malloc(sizeof(struct thread));
+  current_thread = (thread_t)malloc(sizeof(struct thread));
   run_queue = (struct queue *)malloc(sizeof(struct queue));
-  init_thread->base = NULL;
+  current_thread->base = NULL;
+  current_thread->stack_ptr = &current_thread;
 }
 
 void thread_create(void (*f)(void *arg), void *arg, unsigned int stacksize)
+
 {
+  current_thread->status = 1;
+  queue_add(run_queue, current_thread);
+  thread_t new_thread = (thread_t)malloc(sizeof(struct thread));
+  new_thread->function = f;
+  new_thread->stack_ptr = malloc(stacksize);
+  new_thread->status = 0;
+  // TODO what is old and new sp?
+  void *old_ptr = current_thread->stack_ptr;
+  ctx_start(old_ptr, new_thread->stack_ptr);
+  // TODO what is top and bottom?
+  // TODO how do we execute f with arg?
+  // TODO ctx_entry?
+  current_thread = new_thread;
+  f(arg);
 }
 
-void thread_yield() {}
+void thread_yield()
+{
+  if (queue_size(run_queue) == 0)
+  {
+    return;
+  }
 
-void thread_exit() {}
+  current_thread->status = 1;
+  queue_add(run_queue, current_thread);
+  thread_t next_thread = queue_get(run_queue);
+  next_thread->status = 0;
+  ctx_switch(current_thread->stack_ptr, next_thread->stack_ptr);
+  current_thread = next_thread;
+}
+
+void thread_exit()
+{
+  if (queue_size(run_queue) == 0)
+  {
+    exit(0);
+  }
+
+  current_thread->status = 2;
+  thread_t old_thread = current_thread;
+  thread_t next_thread = queue_get(run_queue);
+  next_thread->status = 0;
+  ctx_switch(current_thread->stack_ptr, next_thread->stack_ptr);
+  current_thread = next_thread;
+  // TODO how do we clean up the old thread?
+  free(old_thread);
+}
 
 // Sema functions
 void sema_init(struct sema *sema, unsigned int count) {}
